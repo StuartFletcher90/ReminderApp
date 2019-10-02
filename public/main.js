532aead668a67e0905ffb6bc98c8a83d6776ea8e
Administrator <administrator@CN-6.local>
2019-10-02 16:23:51 +0100

    adding stuff

--- public/main.js
+++ public/main.js
@@ -34,7 +34,7 @@ function newElement() {
   var t = document.createTextNode(inputValue);
   li.appendChild(t);
   if (inputValue === '') {
-    alert("You must write something!");
+    alert("Please write a reminder!");
   } else {
     document.getElementById("myUL").appendChild(li);
   }
